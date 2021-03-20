e11082fd517ae061373d3846f9197eed39506cd2
ali raza <mraliryu@gmail.com>
2021-03-20 14:32:25 +0500

    Added empty base project

---
+++ lib/main.dart
@@ -0,0 +1,20 @@
+import 'package:devfolio/view/homepage/homepage.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'DevFolio',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: HomePage(),
+    );
+  }
+}
